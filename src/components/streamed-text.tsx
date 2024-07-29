7ab4c4e53b6a39c6cf60d1a0af018ab32aa3a045
Bruno Carlos <6951456+brnovasco@users.noreply.github.com>
2024-07-29 09:51:37 -0300

    tsx components as consts

--- src/components/streamed-text.tsx
+++ src/components/streamed-text.tsx
@@ -1,11 +1,14 @@
-export default function StreamedText() {
+const StreamedText = () => {
   return (
     <div
       hx-ext="sse"
       sse-connect="/sse"
       className="container mx-auto p-4 text-center justify-center text-gray-400"
     >
+      <p className="text-2xl font-bold">Server Sent Event</p>
       <p sse-swap="timeUpdate">Waiting for server...</p>
     </div>
   );
-}
+};
+
+export default StreamedText;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,15 @@
 import StreamedText from "../components/streamed-text";
 import Layout from "../layouts";
 
-export default function Home() {
+const Home = () => {
   return (
     <Layout>
       <div class="w-full min-h-[100dvh] grid grid-rows-[auto_1fr_auto]">
         <h1 className="text-3xl font-bold text-white text-center justify-center bg-gray-700 flex p-8">
-          Hello SSE
+          SSE Clock
         </h1>
         <div className="text-white text-center items-center justify-center flex flex-col gap-4">
-          <p>Welcome to your new hono app</p>
+          <p>I'm an SSE clock from the backend</p>
           <StreamedText />
         </div>
         <footer className="text-white text-sm text-center bg-gray-900 p-8">
@@ -19,3 +19,5 @@ export default function Home() {
     </Layout>
   );
 }
+
+export default Home;
