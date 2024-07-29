268c209937ea23a80c156334443f96298e04d014
Bruno Carlos <6951456+brnovasco@users.noreply.github.com>
2024-07-29 14:03:59 -0300

    remove text

--- src/components/streamed-text.tsx
+++ src/components/streamed-text.tsx
@@ -5,7 +5,6 @@ const StreamedText = () => {
       sse-connect="/sse"
       className="container mx-auto p-4 text-center justify-center text-gray-400"
     >
-      <p className="text-2xl font-bold">Server Sent Event</p>
       <p sse-swap="timeUpdate">Waiting for server...</p>
     </div>
   );
